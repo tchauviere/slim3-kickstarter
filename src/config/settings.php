aad847eb5bbcdd3ed85aa280bebca087d66985ae
Thibaud Chauviere <thibaud.chauviere@gmail.com>
2019-05-13 12:20:37 +0200

    // Added dotenv file support in project

--- src/config/settings.php
+++ src/config/settings.php
@@ -3,49 +3,51 @@ return [
     'settings' => [
 
         // Slim 3 settings
-        'displayErrorDetails' => true, // set to false in production
-        'addContentLengthHeader' => true, // Allow the web server to send the content-length header
-        'determineRouteBeforeAppMiddleware' => true, // Needed for middleware route name checking
+        'displayErrorDetails' => (bool)(strtolower(getenv('SLIM3_MODE')) === 'dev'),
+        'addContentLengthHeader' => (bool)(getenv('SLIM3_ROUTE_NAME_IN_MIDDLEWARE') === 'true'),
+        'determineRouteBeforeAppMiddleware' => (bool)(getenv('SLIM3_ADD_CONTENT_LENGTH_HEADER') === 'true'),
 
         // App specific settings
-        'mode' => 'dev', // Or 'prod'
-        'secret' => '@CHANGE_THIS_SECRET@',
-        'lang_path' =>  __DIR__ . '/../../lang',
-        'uploadedFileDir' => __DIR__ . '/../../uploads',
+        'mode' => strtolower(getenv('SLIM3_MODE')),
+        'secret' => getenv('APP_SECRET'),
+        'lang_path' => getenv('APP_LANG_PATH'),
+        'uploadedFileDir' => getenv('APP_UPLOADED_FILE_DIRECTORY'),
 
         // Twig settings
         'twig' => [
-            'tpl_path' => __DIR__ . '/../../templates',
-            'cache_path' => __DIR__ . '/../../cache',
+            'tpl_path' => getenv('TWIG_TPL_PATH'),
+            'cache_path' => getenv('TWIG_CACHE_PATH'),
         ],
 
         // Monolog settings
        'logger' => [
-           'name' => 'slim-app',
-           'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../../logs/app.log',
-           'level' => \Monolog\Logger::DEBUG,
+           'name' => getenv('MONOLOG_NAME'),
+           'path' => getenv('MONOLOG_PATH'),
+           'level' => constant(getenv('MONOLOG_LEVEL')),
        ],
 
         // DB settings
         'db' => [
-            'driver' => 'mysql',
-            'host' => 'localhost',
-            'port' => 3306,
-            'database' => '',
-            'username' => 'root',
-            'password' => '',
-            'charset'   => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
+            'driver' => getenv('DB_DRIVER'),
+            'host' => getenv('DB_HOST'),
+            'port' => (int)getenv('DB_PORT'),
+            'database' => getenv('DB_DATABASE'),
+            'username' => getenv('DB_USERNAME'),
+            'password' => getenv('DB_PASSWORD'),
+            'charset'   => getenv('DB_CHARSET'),
+            'collation' => getenv('DB_COLLATION'),
+            'prefix'    => getenv('DB_PREFIX'),
         ],
 
         // Mailer : Check PHPMailer Documentation to see all option and don't forget to modify dependencies.php according to these change.
         'mailer' => [
-            'smtp' => 'my.smtp.com',
-            'port' => 985,
-            'username' => '',
-            'password' => '',
-            'encryption' => '',
+            'smtp' => getenv('MAILER_SMTP'),
+            'port' => (int)getenv('MAILER_PORT'),
+            'username' => getenv('MAILER_USERNAME'),
+            'password' => getenv('MAILER_PASSWORD'),
+            'encryption' => getenv('MAILER_ENCRYPTION'),
+            'mail_from' => getenv('MAILER_MAIL_FROM'),
+            'name_from' => getenv('MAILER_NAME_FROM'),
         ],
     ],
 ];
