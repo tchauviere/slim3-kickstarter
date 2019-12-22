2e98063e61e75e88b469656564258ce70c3e72ef
Thibaud Chauviere <thibaud.chauviere@gmail.com>
2019-12-22 18:19:37 +0100

    // In cas of crash right from the start, this is now logged with monolog as well

--- public/index.php
+++ public/index.php
@@ -1,4 +1,9 @@
 <?php
+
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+use Monolog\Processor\UidProcessor;
+
 try {
     if (PHP_SAPI == 'cli-server') {
         // To help the built-in PHP dev server, check if the request was actually for
@@ -17,8 +22,14 @@ try {
     // Load environment
     putenv('BASE_DIR='.realpath(__DIR__.'/../'));
     putenv('ROUTER_DIR='.realpath(__DIR__.'/../src/routers/'));
-    $dotenv = \Dotenv\Dotenv::create(getenv('BASE_DIR'));
-    $dotenv->load();
+
+    try {
+        $dotenv = \Dotenv\Dotenv::create(getenv('BASE_DIR'));
+        $dotenv->load();
+    } catch (Exception $e) {
+        throw new Exception('Unable to find .env file !', -999)
+    }
+
 
     // Instantiate the app
     $settings = require __DIR__ . '/../src/config/settings.php';
@@ -33,10 +44,22 @@ try {
     // Run app
     $app->run();
 } catch (Exception $e) {
+
     if (strtolower(getenv('SLIM3_MODE')) === 'dev') {
+        var_dump($e->getMessage());
         var_dump($e->getTrace());
         exit;
     }
+
+    // If error is concerning missing .env, we couldnot add log anyway because logger needs it to be set up.
+    if ($e->getCode() != -999) {
+        $settingLogger = $settings['logger'];
+        $logger = new Logger($settingLogger['name']);
+        $logger->pushProcessor(new UidProcessor());
+        $logger->pushHandler(new StreamHandler($settingLogger['path'], $settingLogger['level']));
+        $logger->addError('INDEX.PHP (Entry Point) : "'.$e->getMessage().'" (CODE : "'.$e->getCode().'")');
+    }
+
     // Force browser to display 500 Error
     throw new Exception('500');
 }
